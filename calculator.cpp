d11de675619075adc82b840bd71f8024c263c56f
alinaa36 <alinakulis8@gmail.com>
2024-11-14 18:03:48 +0200

    Made custom improvements to the calculator

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,24 @@
 #include "calculator.h"
 
-int Calculator::Add(double a, double b)
+double Calculator::Add(double a, double b)
 {
-    return a + b + 0.5; 
+    return a + b;
 }
 
-int Calculator::Sub (double a, double b)
+double Calculator::Sub(double a, double b)
 {
-    return Add (a, -b);
+    return Add(a, -b);  
 }
 
-int Calculator::Mul (double a, double b)
+double Calculator::Mul(double a, double b)
 {
-    return a * b + 0.5;
+    return a * b;  
+}
+
+double Calculator::Div(double a, double b)
+{
+    if (b == 0) {
+        throw std::invalid_argument("Division by zero is not allowed.");
+    }
+    return a / b;  
 }
--- calculator.h
+++ calculator.h
@@ -4,14 +4,10 @@
 class Calculator
 {
     public:
-<<<<<<< HEAD
-        int Add (double, double);  // метод для додавання, використовує double
-        int Sub (double, double);  // метод для віднімання
-        int Mul (double, double);  // метод для множення, додано у новому комітті
-=======
-        static int Add (int, int);
-        static int Mul (double, double);
->>>>>>> d61d32dcee5a4c624759f0966d3b25fe9c06f308
+        double Add(double a, double b);  
+        double Sub(double a, double b);  
+        double Mul(double a, double b);  
+        double Div(double a, double b);  
 };
 
 #endif // CALCULATOR_H
